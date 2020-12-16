8ebde30cd0b41e80265f63c27fb139f2531660e9
Marek Połom <marekpolom22@gmail.com>
2020-12-16 20:05:59 +0100

    zadanie 2

---
+++ tests/test_time.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import *
+
+import sample.time as time
+
+
+class TestTime(unittest.TestCase):
+
+    def test_time(self):
+        #mock
+        test_object = time.Checker()
+        test_object.time.getTime = MagicMock(return_value = 18)
+
+        #test
+        test_object.reminder('file')
+        self.assertTrue(test_object.time.filePlayed)
+
+    def test_time_1(self):
+        #mock
+        test_object = time.Checker()
+        test_object.time.getTime = MagicMock(return_value = 15)
+
+        #test
+        test_object.reminder('file')
+        self.assertFalse(test_object.time.filePlayed)
+
+
+if __name__ == '__main__':
+    unittest.main()
